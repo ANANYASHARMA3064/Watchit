3e111dc427f589aca9e8e5ff422f2e99d884d3e5
Ananya Sharma <ananyasharma3064@gmail.com>
2025-09-06 17:59:11 -0400

    added a Modal

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,36 +1,53 @@
+// Card.jsx
+import { useState } from "react";
 import ShowCard from "./ShowCard";
-export default function Card({movieData,watchMovies,handleAdd,imdbID}){
-    if (!movieData || movieData.length === 0) {
-        return <p>No movies found. Try another search!</p>;
-      }
-         
 
-    return(
-        <div className="parent">
-        <div className="Card">
+export default function Card({ movieData, watchMovies, handleAdd, imdbID }) {
+  const [isShowCard, setIsShowCard] = useState(false);
+  const [selectedMovieID, setSelectedMovieID] = useState(null);
+  if (!movieData || movieData.length === 0) {
+    return <p>No movies found. Try another search!</p>;
+  }
+
+  return (
+    <div className="parent">
+      <div className="Card">
         <div className="movie-container">
- {movieData.map((movie) => (
-        <div key={movie.imdbID} className="movie-card">
-          <img src={movie.Poster !== "N/A" ? movie.Poster : "/placeholder.jpg"} alt={movie.Title} />
-          <h3>{movie.Title}</h3>
-          <p>{movie.Year}</p>
-          <hr></hr>
-         
-          <button className="adding" onClick={() => handleAdd(movie.Title,movie.imdbID)}>
-           {watchMovies.includes(movie.Title)&&imdbID.includes(movie.imdbID) ?
-           (<i class="fa-solid fa-check"></i>)
-  
-  :(<i className="fa-solid fa-plus"></i>)
-  } 
-</button>
-          
+          {movieData.map((movie) => (
+            <div
+              key={movie.imdbID}
+              className="movie-card"
+              onClick={() => {setIsShowCard(true);setSelectedMovieID(movie.imdbID);}} 
+            >
+              <img
+                src={movie.Poster !== "N/A" ? movie.Poster : "/placeholder.jpg"}
+                alt={movie.Title}
+              />
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+              <hr />
+              <button
+                className="adding"
+                onClick={(e) => {
+                  e.stopPropagation(); 
+                  handleAdd(movie.Title, movie.imdbID);
+                }}
+              >
+                {watchMovies.includes(movie.Title) &&
+                imdbID.includes(movie.imdbID) ? (
+                  <i className="fa-solid fa-check"></i>
+                ) : (
+                  <i className="fa-solid fa-plus"></i>
+                )}
+              </button>
+              
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-       
-    </div>
-    </div>
-    )
-    
+        {isShowCard && <ShowCard imdbID={selectedMovieID} onClose={()=>{setIsShowCard(false)}}/>}
+      </div>
 
+      
+    </div>
+  );
 }
--- src/Components/ShowCard.jsx
+++ src/Components/ShowCard.jsx
@@ -1,9 +1,50 @@
+import { useEffect,useState } from "react";
 
+const ShowCard = ({imdbID,onClose}) => {
+    const [movieData, setMovieData] = useState(null);
 
-const ShowCard = () => {
+    const PlotFetcher =async(imdbID)=>{
+        try {
+      
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&i=${imdbID}&plot=full}`
+      );
+      
+      const data = await response.json();
+      setMovieData(data)
+      console.log("plot",data)
+    }catch (err) {
+      console.error("API Error:", err.message);
+      return null;
+    }
+     
+}
+ useEffect(() => {
+    if (imdbID) {
+      PlotFetcher(imdbID);
+    }
+  }, [imdbID]);
   return (
-    <div className="bg-green">
-      <h1>Guddu</h1>
+    <div className="fixed bg-black/60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"onClick={onClose}>
+       
+        <div className="bg-gradient-to-b from-black to-[#090f33] border-2 border-white rounded-2xl shadow-xl p-6 w-[500px] max-w-[90%] text-white">
+            { movieData ?(
+                <>
+
+                 <h2 className="text-xl font-bold mb-2">{movieData.Title}</h2>
+                  <img 
+                src={movieData.Poster !== "N/A" ? movieData.Poster : "/placeholder.jpg"}
+                alt={movieData.Title}
+                className="mx-auto"
+              />
+            <p className="italic mb-2">Released: {movieData.Year}</p>
+            <p className="mb-4">{movieData.Plot}</p>
+            <p className="italic mb-2">Imdb: {movieData.imdbRating}</p>
+                </>
+            ):(
+                <p className="text-gray-500">Loading...</p>
+            )}
+      </div>
     </div>
   )
 }
