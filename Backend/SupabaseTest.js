c1e67f52a4fffbe5cd9ecc83c0ac032bd9803a35
Ananya Sharma <ananyasharma3064@gmail.com>
2025-11-18 00:37:21 -0500

    hopefully fixed something

--- Backend/SupabaseTest.js
+++ Backend/SupabaseTest.js
@@ -2,8 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// const supabaseUrl = 'https://pvsbgpeflbmnkrdtqviq.supabase.co';
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2c2JncGVmbGJtbmtyZHRxdmlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc3MzQ0NzMsImV4cCI6MjA3MzMxMDQ3M30.yP3xcdaK7hJQfJ1yl2d02HNDsNhcs6s5cshr6jTwtq0'; // from Supabase → Settings → API → anon key
 
 const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_KEY);
 
